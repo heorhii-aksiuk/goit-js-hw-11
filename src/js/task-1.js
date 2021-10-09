f1c45d82c774f7ced97bfc29c7d09be382ff651a
Heorhii Aksiuk <aksiuk.heorhii@gmail.com>
2021-10-09 22:20:08 +0300

    Task-1. Create buttons and refs on elements

--- src/js/task-1.js
+++ src/js/task-1.js
@@ -1,2 +1,7 @@
-let test = 'test';
-console.log(test);
+const refs = {
+  bodyEl: document.querySelector('body'),
+  startBtn: document.querySelector('[data-action="start"]'),
+  stopBtn: document.querySelector('[data-action="stop"]'),
+};
+const { bodyEl, startBtn, stopBtn } = refs;
+console.log(bodyEl);
