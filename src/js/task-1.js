8cab9bc765c4eb3edd62574824930b72c7c5cafd
Heorhii Aksiuk <aksiuk.heorhii@gmail.com>
2021-10-09 23:08:22 +0300

    Реализация изменения цвета по кнопке

--- src/js/task-1.js
+++ src/js/task-1.js
@@ -13,3 +13,17 @@ const colors = [
   '#009688',
   '#795548',
 ];
+
+const randomIntegerFromInterval = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+startBtn.addEventListener('click', startBodyColorChange);
+
+function startBodyColorChange() {
+  setInterval(changeColor, 1000);
+}
+function changeColor() {
+  let value = randomIntegerFromInterval(0, colors.length - 1);
+  bodyEl.style.backgroundColor = colors[value];
+}
