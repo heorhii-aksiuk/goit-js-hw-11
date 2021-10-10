14119b319c3814c6bf21e155864d0afba9883103
Heorhii Aksiuk <aksiuk.heorhii@gmail.com>
2021-10-10 23:10:30 +0300

    Delete comments

--- src/js/task-2.js
+++ src/js/task-2.js
@@ -1,22 +1,21 @@
-// /* Задание 1 */
+/* Задание 1 */
 
-// const delay = ms => {
-//   return new Promise(resolve => {
-//     setTimeout(() => {
-//       resolve(ms);
-//     }, ms);
-//   });
-// };
+const delay = ms => {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve(ms);
+    }, ms);
+  });
+};
 
-// const logger = time => console.log(`Resolved after ${time}ms`);
+const logger = time => console.log(`Resolved after ${time}ms`);
 
-// // Вызовы функции для проверки
-// delay(2000).then(logger); // Resolved after 2000ms
-// delay(1000).then(logger); // Resolved after 1000ms
-// delay(1500).then(logger); // Resolved after 1500ms
+// Вызовы функции для проверки
+delay(2000).then(logger); // Resolved after 2000ms
+delay(1000).then(logger); // Resolved after 1000ms
+delay(1500).then(logger); // Resolved after 1500ms
 
-/* Задание 2
-Перепиши функцию toggleUserState() так, чтобы она не использовала callback-функцию callback, а принимала всего два параметра allUsers и userName и возвращала промис. */
+/* Задание 2 */
 
 const users = [
   { name: 'Mango', active: true },
@@ -36,14 +35,5 @@ const toggleUserState = (allUsers, userName) => {
 
 const logger = updatedUsers => console.table(updatedUsers);
 
-/*
- * Сейчас работает так
- */
-// toggleUserState(users, 'Mango', logger);
-// toggleUserState(users, 'Lux', logger);
-
-/*
- * Должно работать так
- */
 toggleUserState(users, 'Mango').then(logger);
 toggleUserState(users, 'Lux').then(logger);
