52a481cfafa00ab7f7f54bc4722546d8193ece13
Heorhii Aksiuk <aksiuk.heorhii@gmail.com>
2021-10-10 15:09:34 +0300

    Реализация остановки изменения цвета по нажатию на кнопку "Stop"

--- src/js/task-1.js
+++ src/js/task-1.js
@@ -14,15 +14,23 @@ const colors = [
   '#795548',
 ];
 
+let intervalId = null;
+
 const randomIntegerFromInterval = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1) + min);
 };
 
 startBtn.addEventListener('click', startBodyColorChange);
+stopBtn.addEventListener('click', stopBodyColorChange);
 
 function startBodyColorChange() {
-  setInterval(changeColor, 1000);
+  intervalId = setInterval(changeColor, 1000);
 }
+
+function stopBodyColorChange() {
+  clearInterval(intervalId);
+}
+
 function changeColor() {
   let value = randomIntegerFromInterval(0, colors.length - 1);
   bodyEl.style.backgroundColor = colors[value];
