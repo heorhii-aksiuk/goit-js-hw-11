d43c442b3778cc1b6913ec18593952a17970272c
Heorhii Aksiuk <aksiuk.heorhii@gmail.com>
2021-10-10 23:08:22 +0300

    Решение задачи 2, задачи 2

--- src/js/task-2.js
+++ src/js/task-2.js
@@ -1,16 +1,49 @@
-/* Задание 1 */
+// /* Задание 1 */
 
-const delay = ms => {
+// const delay = ms => {
+//   return new Promise(resolve => {
+//     setTimeout(() => {
+//       resolve(ms);
+//     }, ms);
+//   });
+// };
+
+// const logger = time => console.log(`Resolved after ${time}ms`);
+
+// // Вызовы функции для проверки
+// delay(2000).then(logger); // Resolved after 2000ms
+// delay(1000).then(logger); // Resolved after 1000ms
+// delay(1500).then(logger); // Resolved after 1500ms
+
+/* Задание 2
+Перепиши функцию toggleUserState() так, чтобы она не использовала callback-функцию callback, а принимала всего два параметра allUsers и userName и возвращала промис. */
+
+const users = [
+  { name: 'Mango', active: true },
+  { name: 'Poly', active: false },
+  { name: 'Ajax', active: true },
+  { name: 'Lux', active: false },
+];
+
+const toggleUserState = (allUsers, userName) => {
   return new Promise(resolve => {
-    setTimeout(() => {
-      resolve(ms);
-    }, ms);
+    const updatedUsers = allUsers.map(user =>
+      user.name === userName ? { ...user, active: !user.active } : user,
+    );
+    resolve(updatedUsers);
   });
 };
 
-const logger = time => console.log(`Resolved after ${time}ms`);
+const logger = updatedUsers => console.table(updatedUsers);
 
-// Вызовы функции для проверки
-delay(2000).then(logger); // Resolved after 2000ms
-delay(1000).then(logger); // Resolved after 1000ms
-delay(1500).then(logger); // Resolved after 1500ms
+/*
+ * Сейчас работает так
+ */
+// toggleUserState(users, 'Mango', logger);
+// toggleUserState(users, 'Lux', logger);
+
+/*
+ * Должно работать так
+ */
+toggleUserState(users, 'Mango').then(logger);
+toggleUserState(users, 'Lux').then(logger);
