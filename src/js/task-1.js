64e1319a303af5017c0f86027bbf49addd702866
Heorhii Aksiuk <aksiuk.heorhii@gmail.com>
2021-10-10 15:25:28 +0300

    Деактивация кнопок и рефакторинг

--- src/js/task-1.js
+++ src/js/task-1.js
@@ -14,20 +14,25 @@ const colors = [
   '#795548',
 ];
 
-let intervalId = null;
+startBtn.style.cursor = 'pointer';
+stopBtn.style.cursor = 'pointer';
 
-const randomIntegerFromInterval = (min, max) => {
-  return Math.floor(Math.random() * (max - min + 1) + min);
-};
+stopBtn.disabled = true;
+
+let intervalId = null;
 
 startBtn.addEventListener('click', startBodyColorChange);
 stopBtn.addEventListener('click', stopBodyColorChange);
 
 function startBodyColorChange() {
+  startBtn.disabled = true;
+  stopBtn.disabled = false;
   intervalId = setInterval(changeColor, 1000);
 }
 
 function stopBodyColorChange() {
+  startBtn.disabled = false;
+  stopBtn.disabled = true;
   clearInterval(intervalId);
 }
 
@@ -35,3 +40,7 @@ function changeColor() {
   let value = randomIntegerFromInterval(0, colors.length - 1);
   bodyEl.style.backgroundColor = colors[value];
 }
+
+const randomIntegerFromInterval = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
