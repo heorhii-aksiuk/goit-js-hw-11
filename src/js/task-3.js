18f842b89e016aa4510c574883799aa12cdf4c7a
Heorhii Aksiuk <aksiuk.heorhii@gmail.com>
2021-10-11 19:33:29 +0300

    Created markup and refs

--- src/js/task-3.js
+++ src/js/task-3.js
@@ -1,1 +1,9 @@
-console.log('test');
+const refs = {
+  timerEl: document.getElementById('timer-1'),
+  daysEl: document.querySelector('[data-value="days"]'),
+  hoursEl: document.querySelector('[data-value="hours"]'),
+  minsEl: document.querySelector('[data-value="mins"]'),
+  secsEl: document.querySelector('[data-value="secs"]'),
+};
+
+const { timerEl, daysEl, hoursEl, minsEl, secsEl } = refs;
