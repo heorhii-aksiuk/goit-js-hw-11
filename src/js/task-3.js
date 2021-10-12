de82e17703f3e3ec2bef489bb4b6ee2ecaec33f7
Heorhii Aksiuk <aksiuk.heorhii@gmail.com>
2021-10-12 15:33:08 +0300

    Падизация))

--- src/js/task-3.js
+++ src/js/task-3.js
@@ -1,5 +1,4 @@
 const refs = {
-  timerEl: document.getElementById('timer-1'),
   daysEl: document.querySelector('[data-value="days"]'),
   hoursEl: document.querySelector('[data-value="hours"]'),
   minsEl: document.querySelector('[data-value="mins"]'),
@@ -18,12 +17,18 @@ class CountdownTimer {
     setInterval(() => {
       const currentDate = Date.now();
       const timeUntil = this.targetDate - currentDate;
-      const days = Math.floor(timeUntil / (1000 * 60 * 60 * 24));
-      const hours = Math.floor(
-        (timeUntil % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60),
-      );
-      const mins = Math.floor((timeUntil % (1000 * 60 * 60)) / (1000 * 60));
-      const secs = Math.floor((timeUntil % (1000 * 60)) / 1000);
+      const days = String(
+        Math.floor(timeUntil / (1000 * 60 * 60 * 24)),
+      ).padStart(2, '0');
+      const hours = String(
+        Math.floor((timeUntil % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)),
+      ).padStart(2, '0');
+      const mins = String(
+        Math.floor((timeUntil % (1000 * 60 * 60)) / (1000 * 60)),
+      ).padStart(2, '0');
+      const secs = String(
+        Math.floor((timeUntil % (1000 * 60)) / 1000),
+      ).padStart(2, '0');
       daysEl.textContent = days;
       hoursEl.textContent = hours;
       minsEl.textContent = mins;
@@ -37,6 +42,4 @@ const courseFinishTimer = new CountdownTimer({
   targetDate: new Date('Feb 13, 2022'),
 });
 
-courseFinishTimer.start;
-
-console.log(courseFinishTimer.start());
+courseFinishTimer.start();
