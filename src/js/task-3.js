7e0db86d92123b98ad4c25fa890d5bf4d2bf454b
Heorhii Aksiuk <aksiuk.heorhii@gmail.com>
2021-10-12 13:51:23 +0300

    Реализация задачи 3 без класса и без интервала

--- src/js/task-3.js
+++ src/js/task-3.js
@@ -8,12 +8,25 @@ const refs = {
 
 const { timerEl, daysEl, hoursEl, minsEl, secsEl } = refs;
 const courseFinishDate = new Date('Feb 13, 2022');
+const currentDate = Date.now();
+const timeUntil = courseFinishDate - currentDate;
+const days = Math.floor(timeUntil / (1000 * 60 * 60 * 24));
+const hours = Math.floor(
+  (timeUntil % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60),
+);
+const mins = Math.floor((timeUntil % (1000 * 60 * 60)) / (1000 * 60));
+const secs = Math.floor((timeUntil % (1000 * 60)) / 1000);
 
-class CountdownTimer {
-  constructor(selector, targetDate) {
-    this.selector = selector;
-    this.targetDate = targetDate;
-  }
-}
+daysEl.textContent = days;
+hoursEl.textContent = hours;
+minsEl.textContent = mins;
+secsEl.textContent = secs;
 
-const courseFinishTimer = new CountdownTimer(timerEl, courseFinishDate);
+// class CountdownTimer {
+//   constructor(selector, targetDate) {
+//     this.selector = selector;
+//     this.targetDate = targetDate;
+//   }
+// }
+
+// const courseFinishTimer = new CountdownTimer(timerEl, courseFinishDate);
