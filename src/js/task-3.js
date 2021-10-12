5fff7693b589b95248cabf642fe54a2eac9cabbc
Heorhii Aksiuk <aksiuk.heorhii@gmail.com>
2021-10-12 14:00:43 +0300

    Реализация задачи 3 через грязнейшую функцию

--- src/js/task-3.js
+++ src/js/task-3.js
@@ -5,22 +5,25 @@ const refs = {
   minsEl: document.querySelector('[data-value="mins"]'),
   secsEl: document.querySelector('[data-value="secs"]'),
 };
-
 const { timerEl, daysEl, hoursEl, minsEl, secsEl } = refs;
-const courseFinishDate = new Date('Feb 13, 2022');
-const currentDate = Date.now();
-const timeUntil = courseFinishDate - currentDate;
-const days = Math.floor(timeUntil / (1000 * 60 * 60 * 24));
-const hours = Math.floor(
-  (timeUntil % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60),
-);
-const mins = Math.floor((timeUntil % (1000 * 60 * 60)) / (1000 * 60));
-const secs = Math.floor((timeUntil % (1000 * 60)) / 1000);
 
-daysEl.textContent = days;
-hoursEl.textContent = hours;
-minsEl.textContent = mins;
-secsEl.textContent = secs;
+function updateTime() {
+  const courseFinishDate = new Date('Feb 13, 2022');
+  const currentDate = Date.now();
+  const timeUntil = courseFinishDate - currentDate;
+  const days = Math.floor(timeUntil / (1000 * 60 * 60 * 24));
+  const hours = Math.floor(
+    (timeUntil % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60),
+  );
+  const mins = Math.floor((timeUntil % (1000 * 60 * 60)) / (1000 * 60));
+  const secs = Math.floor((timeUntil % (1000 * 60)) / 1000);
+  daysEl.textContent = days;
+  hoursEl.textContent = hours;
+  minsEl.textContent = mins;
+  secsEl.textContent = secs;
+}
+
+setInterval(updateTime, 1000);
 
 // class CountdownTimer {
 //   constructor(selector, targetDate) {
